e41457800c5f09c0e69a3baf79e100b5775fa5d3
c2phi-prof <prof.c2phi.fr@gmail.com>
2026-05-05 11:10:43 +0200

    Update content.js

--- content.js
+++ content.js
@@ -6,21 +6,21 @@ const blogData = [
         sections: [
             {
                 section: "Calliclès",
-                text: "Le droit positif n'est qu'une invention du peuple contre l'aristocratie. La véritable justice réside dans la puissance et l'excès de force. Il n'y a de légitimité que dans l'ordre de la nature, là où le fort domine faible. Les lois ne sont qu'un artifice pour renverser le pouvoir des forts."
+                text: "Le droit positif n'est qu'une invention du peuple contre l'aristocratie. La véritable justice réside dans la puissance et l'excès de force. Il n'y a de légitimité que dans l'ordre de la nature, là où le fort domine le faible. Les lois ne sont qu'un artifice pour renverser le pouvoir des forts."
             },
             {
                                section: "Caligula",
-                text: "Le pouvoir absolu n'a pas l'ordre pour finalité, mais la révélation de l'absurdité du monde. En exigeant l'impossible, le tyran déchire le contrat social pour exposer la nudité de l'existence. Le droit s'anéantit devant une volonté qui se veut l'égale du destin. La loi n'est plus qu'une mise en scène. Elle démontre que la seule liberté réside dans la destruction de tous forme de justice par une [logique de la démesure|https://gallimard.fr/Catalogue/GALLIMARD/Folio/Folio-theatre/Caligula] poussée jusqu'au néant."
+                text: "Le pouvoir absolu n'a pas pour but d'imposer l'ordre, mais de révéler l'absurdité du monde. En exigeant l'impossible, le tyran déchire le contrat social pour exposer la nudité de l'existence. Le droit s'anéantit devant une volonté qui se veut l'égale du destin. La loi n'est plus qu'une mise en scène. Elle démontre que la seule liberté réside dans la destruction de toute forme de justice par une [logique de la démesure|https://gallimard.fr/Catalogue/GALLIMARD/Folio/Folio-theatre/Caligula] poussée jusqu'au néant."
             },
             {
                
                 section: "Dark Vador",
-                text: "Le droit est un symptôme de faiblesse. L'ordre galactique exige l'unification par le pouvoir absolu de la technique. La Force, en tant que pouvoir obscure, transcende les tergiversations de la justice et du droit pour instaurer une paix fondée uniquement sur la peur. L'efficacité de la terreur rend superflue l'usage du droit. Seul le maintien absolu de la stabilité de l'[Empire|https://www.jstor.org/stable/j.ctt1v27sq1] par une force implacble peut être appelé juste."
+                text: "Le droit est un symptôme de faiblesse. L'ordre galactique exige l'unification par le pouvoir absolu de la technique. La Force, en tant que pouvoir obscur, transcende les tergiversations de la justice et du droit, pour instaurer une paix fondée uniquement sur la peur. L'efficacité de la terreur rend superflue l'usage du droit. Seul le maintien absolu de la stabilité de l'[Empire|https://www.jstor.org/stable/j.ctt1v27sq1] par une force implacble peut être appelé juste."
             },
             {
                
                 section: "Rousseau",
-                text: "On ne cède à la force que par nécessité, non par volonté. Puisqu'aucune raison morale ne légitimie la force, nous n'avons pas d'obligation envers elle. En revanche la supériorité du droit réside dans l'expression de la volonté générale transmise par le pacte social. Un peuple qui n'obéit que par force n'a pas de devoirs, il n'a que des chaînes. Mais l'aliénation totale de chaque associé à la communauté permet de [transformer la force en droit|https://gallica.bnf.fr/ark:/12148/bpt6k71164d]."
+                text: "On ne cède à la force que par nécessité, non par volonté. Puisqu'aucune raison morale ne légitimie la force, nous n'avons pas d'obligation envers elle. En revanche, la supériorité du droit réside dans l'expression de la volonté générale fondée sur le pacte social. Un peuple qui n'obéit que par force n'a pas de devoirs, il n'a que des chaînes. Mais l'aliénation totale de chaque associé à la communauté entière permet de [transformer la force en droit|https://cours-et-fiches.com/livres/du-contrat-social-rousseau/]."
             },
             {
                section: "Max Weber",
